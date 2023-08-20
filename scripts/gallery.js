439a7fa16ef0e7377f772ef9113313318be3df0d
Norbert Prucnal <nor.bert.prucnal@gmail.com>
2023-08-20 22:10:40 +0200

    added js to open gallery

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,14 +1,22 @@
-{
-  function openGallery(imgs) {
-    let expandImg = document.getElementById("expandedImg");
-    let photoblock = document.querySelector(".photoView");
-    let closeBtn = document.querySelector(".closeBtn");
+function openGallery(imgs) {
+  let expandImg = document.getElementById("expandedImg");
+  let photoblock = document.querySelector(".photoView");
+  let closeBtn = document.querySelector(".closeBtn");
 
-    expandImg.src = imgs.src;
-    expandImg.parentElement.style.display = "flex";
+  expandImg.src = imgs.src;
+  expandImg.parentElement.style.display = "flex";
 
-    closeBtn.addEventListener("click", () => {
-      photoblock.style.display = "none";
-    });
-  }
+  closeBtn.addEventListener("click", () => {
+    photoblock.style.display = "none";
+  });
 }
+
+const moreBtn = document.querySelector(".button-gallery");
+const gallery = document.querySelector(".grid");
+const overlay = document.querySelector(".grid-overlay");
+
+moreBtn.addEventListener("click", () => {
+  gallery.style.maxHeight = "none";
+  overlay.style.display = "none";
+  moreBtn.style.display = "none";
+});
