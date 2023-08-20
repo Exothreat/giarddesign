86ac8836c6b3229f6d7564d234b44c2c83881f58
Norbert Prucnal <nor.bert.prucnal@gmail.com>
2023-08-20 22:27:59 +0200

    working buttons for gallery

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -11,12 +11,21 @@ function openGallery(imgs) {
   });
 }
 
-const moreBtn = document.querySelector(".button-gallery");
+const moreBtn = document.querySelector(".more-gallery-button");
 const gallery = document.querySelector(".grid");
 const overlay = document.querySelector(".grid-overlay");
+const lessBtn = document.querySelector(".less-gallery-button");
 
 moreBtn.addEventListener("click", () => {
   gallery.style.maxHeight = "none";
   overlay.style.display = "none";
   moreBtn.style.display = "none";
+  lessBtn.style.display = "block";
+});
+
+lessBtn.addEventListener("click", () => {
+  gallery.style.maxHeight = "1510px";
+  overlay.style.display = "block";
+  lessBtn.style.display = "none";
+  moreBtn.style.display = "block";
 });
